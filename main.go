fc889511775a42640d5dd71dcce4d0c2d2f83dec
unknown <ayanemikane@gmail.com>
2023-12-13 21:43:51 +0900

    new changes has approved

--- main.go
+++ main.go
@@ -13,97 +13,22 @@ var (
 	createUserRe = regexp.MustCompile(`^\/users[\/]*$`)
 )
 
-type UserHandler struct {
-	store *DataStore
-}
-
-type User struct {
+type user struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
-type DataStore struct {
-	m map[string]User
+type datastore struct {
+	m map[string]user
 	*sync.RWMutex
 }
 
-func (h *UserHandler) List(w http.ResponseWriter, r *http.Request) {
-	h.store.RLock()
-	users := make([]User, 0, len(h.store.m))
-	for _, v := range h.store.m {
-		users = append(users, v)
-	}
-
-	h.store.RUnlock()
-	jsonBytes, err := json.Marshal(users)
-	if err != nil {
-		internalServerError(w, r)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
+type userHandler struct {
+	store *datastore
 }
 
-func (h *UserHandler) Get(w http.ResponseWriter, r *http.Request) {
-	matches := getUserRe.FindStringSubmatch(r.URL.Path)
-	if len(matches) < 2 {
-		notFound(w, r)
-		return
-	}
-
-	h.store.RLock()
-	u, ok := h.store.m[matches[1]]
-	h.store.RUnlock()
-
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("USER NOT FOUND"))
-		return
-	}
-
-	jsonBytes, err := json.Marshal(u)
-	if err != nil {
-		internalServerError(w, r)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-}
-
-func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
-	var user User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		internalServerError(w, r)
-		return
-	}
-
-	h.store.Lock()
-	h.store.m[user.ID] = user
-	h.store.Unlock()
-	jsonBytes, err := json.Marshal(user)
-
-	if err != nil {
-		internalServerError(w, r)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-}
-
-func internalServerError(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte("INTERNAL SERVER ERROR"))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("NOT FOUND"))
-}
-
-func (h *UserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+func (h *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 	switch {
 	case r.Method == http.MethodGet && listUserRe.MatchString(r.URL.Path):
 		h.List(w, r)
@@ -120,18 +45,85 @@ func (h *UserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (h *userHandler) List(w http.ResponseWriter, r *http.Request) {
+	h.store.RLock()
+	users := make([]user, 0, len(h.store.m))
+	for _, v := range h.store.m {
+		users = append(users, v)
+	}
+	h.store.RUnlock()
+	jsonBytes, err := json.Marshal(users)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *userHandler) Get(w http.ResponseWriter, r *http.Request) {
+	matches := getUserRe.FindStringSubmatch(r.URL.Path)
+	if len(matches) < 2 {
+		notFound(w, r)
+		return
+	}
+	h.store.RLock()
+	u, ok := h.store.m[matches[1]]
+	h.store.RUnlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("user not found"))
+		return
+	}
+	jsonBytes, err := json.Marshal(u)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (h *userHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var u user
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		internalServerError(w, r)
+		return
+	}
+	h.store.Lock()
+	h.store.m[u.ID] = u
+	h.store.Unlock()
+	jsonBytes, err := json.Marshal(u)
+	if err != nil {
+		internalServerError(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("internal server error"))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("not found"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	userH := &UserHandler{
-		store: &DataStore{
-			m: map[string]User{
-				"1": {ID: "1", Name: "Lolen"},
+	userH := &userHandler{
+		store: &datastore{
+			m: map[string]user{
+				"1": user{ID: "1", Name: "bob"},
 			},
 			RWMutex: &sync.RWMutex{},
 		},
 	}
-	// mux.Handle("/users", &UserHandler{})
 	mux.Handle("/users", userH)
 	mux.Handle("/users/", userH)
-	http.ListenAndServe(":8080", mux)
+
+	http.ListenAndServe("localhost:8080", mux)
 }
